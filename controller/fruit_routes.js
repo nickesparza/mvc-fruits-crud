e47d6d60141ccf87ed18d91cbcd3bd575eae1d67
nickesparza <nickesparza@gmail.com>
2022-07-07 14:23:22 -0500

    new and edit routes

--- controller/fruit_routes.js
+++ controller/fruit_routes.js
@@ -4,6 +4,50 @@ const router = express.Router()
 // import Fruit model to access database
 const Fruit = require('../models/fruit')
 
+// GET - Update form for changing fruits
+router.get('/:id/edit', (req, res) => {
+    // assign id from request parameter to a variable
+    const fruitId = req.params.id
+    // find that id in the database and render the edit page
+    Fruit.findById(fruitId)
+        .then(fruit => {
+            res.render('fruits/edit', { fruit })
+        })
+        .catch(err => res.json(err))
+})
+
+// PUT - Update route for changing an existing fruit
+router.put('/:id', (req, res) => {
+    const fruitId = req.params.id
+    // set checkbox from on/off to true/false
+    req.body.readyToEat = req.body.readyToEat === 'on'
+    // findbyIdAndUpdate uses the fruitId, then updates it with the request body, and uses new: true since this is a put request
+    Fruit.findByIdAndUpdate(fruitId, req.body, { new: true })
+        .then(fruit => {
+            res.redirect(`/fruits/${fruit.id}`)
+        })
+        .catch(err => {res.json(err)})
+})
+
+// GET - Create form for new fruits
+router.get('/new', (req, res) => {
+    res.render('fruits/new')
+})
+
+// POST - Add new fruit to database
+router.post('/', (req, res) => {
+    // set checkbox from on/off to true/false
+    req.body.readyToEat = req.body.readyToEat === 'on'
+    // create item in database using request body
+    Fruit.create(req.body)
+        .then(fruit => {
+            console.log(fruit)
+            // res.json(fruit)
+            res.redirect('/fruits')
+        })
+        .catch(err => res.json(err))
+})
+
 // GET - Index
 // localhost:3000/fruits
 router.get('/', (req, res) => {
@@ -25,7 +69,10 @@ router.get('/:id', (req, res) => {
     const fruitId = req.params.id
     // mongoose findById
     Fruit.findById(fruitId)
-        .then(fruit => {res.json(fruit)})
+        .then(fruit => {
+            // res.json(fruit)
+            res.render('fruits/show', {fruit})
+        })
         .catch(err => {res.json(err)})
 })
 
