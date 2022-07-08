86f586bf4ef0f990d5df2648071cf5d1df87ec40
nickesparza <nickesparza@gmail.com>
2022-07-08 13:38:17 -0500

    add signup page and route

--- controller/user_routes.js
+++ controller/user_routes.js
@@ -15,7 +15,30 @@ router.get('/signup', (req, res) => {
     res.render('users/signup')
 })
 // POST to make db request
-// router.post('/users')
+router.post('/signup', async (req, res) => {
+    console.log('this is our request body', req.body)
+    // encrypt password
+    // that is the reason for making this function async
+    // password hashing takes some time, so wait until it's complete before progressing to the next steps
+    // use bcrypt to encrypt password
+    // wait for bcrypt to run salt rounds before continuing
+    // salt rounds - encrypt X amount of times before settling on one encryption
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+    console.log('this is our request body after hash', req.body)
+
+    // now that the password is hashed, create user
+    User.create(req.body)
+        // if created successfully, redirect to login page
+        .then(user => {
+            console.log('this is the new user', user)
+            res.redirect('/users/login')
+        })
+        // if creation fails, send error
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+})
 
 // two login routes
 // GET to show signin form
